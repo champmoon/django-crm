8a6dab1a10d1b1fdb66e551d93f4c6f5aced4ef6
CHAMPMOON <antipov8v@gmail.com>
2022-06-05 19:25:58 +0300

    register User, Lead, Agent in admin panel

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Agent, Lead, User
+
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Agent)
+admin.site.register(Lead)
