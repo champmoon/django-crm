cbbe2bab98a6b5d90b6b0205bbe54b115bbd18e3
CHAMPMOON <antipov8v@gmail.com>
2022-06-09 18:05:38 +0300

    create leads views

--- leads/views.py
+++ leads/views.py
@@ -1,1 +1,20 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from .models import Lead
+
+
+def lead_list(request: HttpRequest) -> HttpResponse:
+    leads = Lead.objects.all()
+    context = {
+        'leads': leads
+    }
+    return render(request, 'leads/lead_list.html', context)
+
+
+def lead_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead': lead
+    }
+    return render(request, 'leads/lead_detail.html', context)
