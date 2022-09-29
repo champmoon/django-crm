c52f4fb8e733171ac1005942ef3c4bed9d88c5f3
champmoon <antipov8v@gmail.com>
2022-09-29 20:59:47 +0300

    add agent detail, update and delete views

--- agents/views.py
+++ agents/views.py
@@ -27,3 +27,27 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent.save()
 
         return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Agent
+    template_name = 'agents/agent_detail.html'
+    context_object_name = 'agent'
+
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Agent
+    template_name = 'agents/agent_update.html'
+    form_class = AgentModelForm
+
+    def get_success_url(self) -> str:
+        return resolve_url('agents:agent-list')
+
+
+class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Agent
+    template_name = 'agents/agent_delete.html'
+    context_object_name = 'agent'
+
+    def get_success_url(self) -> str:
+        return resolve_url('agents:agent-list')
