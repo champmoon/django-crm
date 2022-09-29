c5f7bfe0bbf7b2534d14354e6e3d8a52ba91c076
champmoon <antipov8v@gmail.com>
2022-09-29 19:24:51 +0300

    create agent app, list and create views

---
+++ agents/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import ModelForm
+from django.http import HttpResponse
+from django.shortcuts import resolve_url
+from django.views import generic
+
+from leads.models import Agent
+
+from .forms import AgentModelForm
+
+
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_name = 'agents/agent_list.html'
+    model = Agent
+
+
+class AgentCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name = 'agents/agent_create.html'
+    form_class = AgentModelForm
+
+    def get_success_url(self) -> str:
+        return resolve_url('agents:agent-list')
+
+    def form_valid(self, form: ModelForm) -> HttpResponse:
+        agent = form.save(commit=False)
+        agent.organisation = self.request.user.userprofile
+        agent.save()
+
+        return super(AgentCreateView, self).form_valid(form)
